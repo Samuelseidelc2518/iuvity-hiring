3aa40da73d4c196d900531f9eb23a8caf05fb241
Samuelseidelc2518 <samuelsc2518@gmail.com>
2023-04-06 18:08:07 -0500

    Integration of dashboard views and configuration

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './pages/home/home.component';
+import { InventoryListComponent } from './pages/inventory-list/inventory-list.component';
+import { InventoryCreateComponent } from './pages/inventory-create/inventory-create.component';
+import { SalesComponent } from './pages/sales/sales.component';
 
 const routes: Routes = [
   {
@@ -11,6 +14,23 @@ const routes: Routes = [
       {
         path: 'home',
         component: HomeComponent,
+      },
+      {
+        path: 'inventory',
+        children: [
+          {
+            path: 'list',
+            component: InventoryListComponent,
+          },
+          {
+            path: 'create',
+            component: InventoryCreateComponent,
+          },
+        ],
+      },
+      {
+        path: 'sales',
+        component: SalesComponent,
       },
       {
         path: '**',
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FadeIn } from '../core/animations/fadeIn.animation';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  animations: FadeIn.animations,
 })
 export class DashboardComponent implements OnInit {
+  /* Variables */
+  sidebarVisible: boolean = true;
+  activeUrl: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.router.events.subscribe((val) => {
+      // see also
+      if (val instanceof NavigationEnd) {
+        console.log(val.url);
+        this.activeUrl = val.url;
+      }
+    });
   }
 
+  ngOnInit(): void {}
+
+  /* Methods */
+  hideSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
 }
